e83c2865ecb22950698bc2becfb4d78c7f137275
Clint Daniels <clint.daniels@sandag.org>
2016-04-15 11:05:13 -0700

    Refactor the allocation engine.

--- etl/sql/process.py
+++ etl/sql/process.py
@@ -6,56 +6,49 @@ from urbansim.models.lcm import unit_choice
 
 urbansim_engine = create_engine(get_connection_string("E:/Apps/urbansim/sandag_urbansim_rebuild/configs/dbconfig.yml", 'urbansim_database'))
 
-def random_allocate_households(housholds, buildings, mgra_id_col, units_col):
+
+def random_allocate_agents_by_geography(agents, containers, geography_id_col, containers_units_col):
+    """Allocate agents (e.g., households, jobs) to a container (e.g., buildings) based
+       on the number of units available in each container. The agent and container unit
+       totals are controled to a geography.
+
+    :param agents: A dataframe with agents to be assigned.
+    :param containers: A dataframe to which the agents will be assigned
+    :param geography_id_col: The column id in both input dataframes for identifying the control geography zones
+    :param containers_units_col: The column in the container dataframe enumerating number of slots in container for agents
+    :type agents: pandas.DataFrame
+    :type containers: pandas.DataFrame
+    :type geography_id_col: string
+    :type containers_units_col: string
+    :return: Summary dataframe of allocation
+    :rtype: pandas.DataFrame
+    """
     audit_df = pd.DataFrame(
-                    data=np.zeros((len(np.unique(households[mgra_id_col])), 3), dtype=np.int)
-                    ,index=np.unique(households[mgra_id_col])
+                    data=np.zeros((len(np.unique(agents[geography_id_col])), 3), dtype=np.int)
+                    ,index=np.unique(agents[geography_id_col])
                     ,columns=['demand','supply','residual'])
     
-    empty_units = buildings[buildings[units_col] > 0][units_col].sort_values(ascending=False)
-    alternatives = buildings[['development_type_id', 'parcel_id', mgra_id_col]]
+    empty_units = containers[containers[containers_units_col] > 0][containers_units_col].sort_values(ascending=False)
+    alternatives = containers[[geography_id_col]]
     alternatives = alternatives.ix[np.repeat(empty_units.index.values, empty_units.values.astype('int'))]
     
-    mgra_agent_counts = households.groupby(mgra_id_col).size()
-    
-    for mgra in np.unique(households[mgra_id_col]):
-        print "Processing MGRA: %s" % (mgra)
-        num_households = mgra_agent_counts[mgra_agent_counts.index.values == mgra].values[0]
-        chooser_ids = households.index[households[mgra_id_col] == mgra].values
-        alternative_ids = alternatives[alternatives[mgra_id_col] == mgra].index.values
+    geography_agent_counts = agents.groupby(geography_id_col).size()
+    zones = len(np.unique(agents[geography_id_col]))
+
+    for idx, geo_zone in enumerate(np.unique(agents[geography_id_col])):
+        print "Processing %s %s of %s (%s = %s)" % (geography_id_col, idx, zones, geography_id_col, geo_zone)
+
+        num_agents = geography_agent_counts[geography_agent_counts.index.values == geo_zone].values[0]
+        chooser_ids = agents.index[agents[geography_id_col] == geo_zone].values
+        alternative_ids = alternatives[alternatives[geography_id_col] == geo_zone].index.values
         probabilities = np.ones(len(alternative_ids))
         num_units = len(alternative_ids)
         choices = unit_choice(chooser_ids, alternative_ids, probabilities)
-        households.loc[chooser_ids, 'building_id'] = choices
-        audit_df.ix[mgra] = [num_households, num_units, num_units - num_households]
+        agents.loc[chooser_ids, containers.index.name] = choices
+        audit_df.ix[geo_zone] = [num_agents, num_units, num_units - num_agents]
     
     return audit_df
 
-def random_allocate_jobs(jobs, buildings, block_id_col, jobs_col):
-        audit_df = pd.DataFrame(
-                    data=np.zeros((len(np.unique(jobs[block_id_col])), 3), dtype=np.int)
-                    ,index=np.unique(jobs[block_id_col])
-                    ,columns=['demand','supply','residual'])
-        
-        empty_units = buildings[buildings[jobs_col] > 0][jobs_col].sort_values(ascending=False)
-        alternatives = buildings[['development_type_id', 'parcel_id', block_id_col]]
-        alternatives = alternatives.ix[np.repeat(empty_units.index.values, empty_units.values.astype('int'))]
-        
-        block_agent_counts = jobs.groupby(block_id_col).size()
-        blocks = len(np.unique(jobs['block_id']))
-        
-        for idx, block in enumerate(np.unique(jobs[block_id_col])):
-            print "Processing Block %s of %s: %s" % (idx, blocks, block)
-            num_jobs = block_agent_counts[block_agent_counts.index.values == block].values[0]
-            chooser_ids = jobs.index[jobs[block_id_col] == block].values
-            alternative_ids = alternatives[alternatives[block_id_col] == block].index.values
-            probabilities = np.ones(len(alternative_ids))
-            num_units = len(alternative_ids)
-            choices = unit_choice(chooser_ids, alternative_ids, probabilities)
-            jobs.loc[chooser_ids, 'building_id'] = choices
-            audit_df.ix[block] = [num_jobs, num_units, num_units - num_jobs]
-
-        return audit_df
 
 def process_households():
 
@@ -76,7 +69,7 @@ def process_households():
     buildings = pd.read_sql(bldgs_sql, urbansim_engine, index_col='building_id')
     households = pd.read_sql(hh_sql, urbansim_engine, index_col='household_id')
 
-    results_df = random_allocate_households(households, buildings, 'mgra', 'residential_units')
+    results_df = random_allocate_agents_by_geography(households, buildings, 'mgra', 'residential_units')
     results_df.to_csv('process_households_results.csv')
     
     if 'mgra' in households.columns:
@@ -90,6 +83,7 @@ def process_households():
         
     households.to_csv('households.csv')
     households.to_sql('households', urbansim_engine, schema='urbansim', if_exists='replace', chunksize=1000)
+
 
 def process_jobs():
     bldgs_sql = """SELECT 
@@ -111,17 +105,19 @@ def process_jobs():
     buildings.ix[buildings.job_spaces.isnull(), 'job_spaces'] = 0
     buildings['job_spaces'] = buildings['job_spaces'].astype('int')
     
-    results_df = random_allocate_jobs(jobs, buildings, 'block_id', 'job_spaces')
+    results_df = random_allocate_agents_by_geography(jobs, buildings, 'block_id', 'job_spaces')
     results_df.to_csv('process_jobs_results.csv')
     
     if 'block_id' in jobs.columns:
         del jobs['block_id']
         
     jobs.ix[jobs.building_id.isnull(), 'building_id'] = -1
+    buildings['building_id'].astype('int')
     
     jobs.to_csv('jobs.csv')
+    jobs.to_sql('jobs', urbansim_engine, schema='urbansim', if_exists='replace', chunksize=1000)
     
 
 if __name__ == '__main__':
-    #process_households()
+    process_households()
     process_jobs()
