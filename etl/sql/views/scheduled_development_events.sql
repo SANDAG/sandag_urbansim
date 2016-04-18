cd643e4ea10109ed0db664ace1f5a7a302889296
Clint Daniels <clint.daniels@sandag.org>
2016-04-18 11:22:58 -0700

    (ISAM-10) Clean-up table name misspelling.

--- etl/sql/views/scheduled_development_events.sql
+++ etl/sql/views/scheduled_development_events.sql
@@ -5,7 +5,7 @@ IF OBJECT_ID('urbansim.scheduled_development_event') IS NOT NULL
     DROP TABLE urbansim.scheduled_development_event
 GO
 
-CREATE TABLE urbansim.schdeduled_development_event (
+CREATE TABLE urbansim.scheduled_development_event (
   scheduled_development_event_id int not null primary key
   ,year_built smallint not null
   ,sqft_per_unit int not null
@@ -14,7 +14,7 @@ CREATE TABLE urbansim.schdeduled_development_event (
 )
 GO
 
-INSERT INTO urbansim.schdeduled_development_event 
+INSERT INTO urbansim.scheduled_development_event 
     (scheduled_development_event_id, year_built, 
     sqft_per_unit, residential_units, non_residential_sqft)
   SELECT
