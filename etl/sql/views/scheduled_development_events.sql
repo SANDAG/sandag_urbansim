ea548a1ef74e00dea6b16b8fadca53d50c152d20
Clint Daniels <clint.daniels@sandag.org>
2016-04-18 13:54:23 -0700

    (ISAM-10) Adding additional columns to the table schema and dummy load.

--- etl/sql/views/scheduled_development_events.sql
+++ etl/sql/views/scheduled_development_events.sql
@@ -7,22 +7,30 @@ GO
 
 CREATE TABLE urbansim.scheduled_development_event (
   scheduled_development_event_id int not null primary key
+  ,parcel_id int not null
+  ,development_type_id int not null
   ,year_built smallint not null
   ,sqft_per_unit int not null
   ,residential_units int not null
   ,non_residential_sqft int not null
+  ,stories int not null
 )
 GO
 
 INSERT INTO urbansim.scheduled_development_event 
-    (scheduled_development_event_id, year_built, 
-    sqft_per_unit, residential_units, non_residential_sqft)
+    (scheduled_development_event_id, parcel_id, development_type_id
+	,year_built, sqft_per_unit, residential_units, non_residential_sqft
+	,stories)
   SELECT
     siteid
-    ,phase
+    ,p.parcel_id
+	,placetype
+	,phase
     ,sqft_prunt
     ,res_unit
     ,nonres_sqf
+	,avg_story
   FROM
     input.site_spec_stg
+    INNER JOIN urbansim.parcels p ON geom.STCentroid().STIntersects(p.shape) = 1
 GO
