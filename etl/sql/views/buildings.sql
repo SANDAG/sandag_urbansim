75cf50d2ca4388eda52b62e26e5986ed4f5f0325
Elias Sanz <esa@sandag.org>
2016-04-19 15:31:11 -0700

    update residential_sqft, non_residential_sqft and stories from costar

--- etl/sql/views/buildings.sql
+++ etl/sql/views/buildings.sql
@@ -2,31 +2,32 @@ USE spacecore
 IF OBJECT_ID('urbansim.buildings') IS NOT NULL
     DROP TABLE urbansim.buildings
 GO
-CREATE TABLE urbansim.buildings (
-    id  int IDENTITY(1,1)
-	,building_id int NOT NULL
-	,development_type_id int
+CREATE TABLE urbansim.buildings(
+	building_id int IDENTITY(1,1) NOT NULL
+	,development_type_id smallint
 	,parcel_id int
 	,improvement_value float
-	,residential_units int
+	,residential_units tinyint
 	,residential_sqft int
 	,non_residential_sqft int
+    ,job_spaces smallint
+    ,non_residential_rent_per_sqft float
 	,price_per_sqft float
 	,stories int
 	,year_built smallint
 	,shape geometry
 )
-INSERT INTO urbansim.buildings WITH (TABLOCK) (building_id, shape)
+INSERT INTO urbansim.buildings WITH (TABLOCK) (shape)
 SELECT 
-    bldgID
-	,shape
+	shape
+    --bldgID	--NOT UNIQUE ID
 FROM gis.buildings
 
 --CREATE A PRIMARY KEY SO WE CAN CREATE A SPATIAL INDEX
-ALTER TABLE urbansim.buildings ADD CONSTRAINT pk_urbansim_buildings_id PRIMARY KEY CLUSTERED (id) 
+ALTER TABLE urbansim.buildings ADD CONSTRAINT pk_urbansim_buildings_building_id PRIMARY KEY CLUSTERED (building_id) 
 
 --SET THE SHAPES TO BE NOT NULL SO WE CAN CREATE A SPATIAL INDEX
-    ALTER TABLE urbansim.buildings ALTER COLUMN shape geometry NOT NULL
+ALTER TABLE urbansim.buildings ALTER COLUMN shape geometry NOT NULL
 
 --SELECT max(x_coord), min(x_coord), max(y_coord), min(y_coord) from gis.parcels
 
@@ -36,7 +37,6 @@ CREATE SPATIAL INDEX [ix_spatial_urbansim_buildings] ON urbansim.buildings
 ) USING  GEOMETRY_GRID
     WITH (BOUNDING_BOX =(6152300, 1775400, 6613100, 2129400), GRIDS =(LEVEL_1 = MEDIUM,LEVEL_2 = MEDIUM,LEVEL_3 = MEDIUM,LEVEL_4 = MEDIUM), 
     CELLS_PER_OBJECT = 16, PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-
 
 /** LANDCORE DATA **/
 --GET LANDCORE DATA: PARCEL_ID, DEV_TYPE
@@ -52,36 +52,22 @@ JOIN ref.development_type_lu_code dev
 ON lc.lu = dev.lu_code
 WHERE usb.Shape.STCentroid().STWithin(lc.Shape) = 1
 
-
-/** PARCEL DATA **/
---GET PARCEL DATA: RESIDENTIAL UNITS, IMPROVEMENT VALUE,
-UPDATE
-	usb
-SET
-	usb.improvement_value = (i.impr/b.bldgs)
-	--,usb.residential_units = (u.unitqty/b.bldgs)
-FROM
-	urbansim.buildings usb
-	LEFT JOIN 
-		(SELECT parcel_id,  COUNT(parcel_id) bldgs	--NUMBER OF BUILDINGS
-		FROM urbansim.buildings
-		--WHERE parcel_id = 219874		--TEST
-		GROUP BY parcel_id) b
-	 ON usb.parcel_id = b.parcel_id
-	LEFT JOIN 
-		(SELECT parcelid, SUM(asr_impr) impr		--CHECK FIELD NAME
-		FROM GIS.parcels
-		--WHERE PARCELID = 722555		--TEST
-		GROUP BY parcelid) i
-	ON usb.parcel_id = i.parcelid 
-
-/** ASSESSOR DATA **/
+/** ASSESSOR AND LANDCORE DATA **/
 --GET ASSESSOR DATA: RESIDENTIAL UNITS, NON-RES SQFT, PRICE/SQFT, YEAR BUILT
 UPDATE
 	usb
 SET
-	usb.residential_units = (ROUND((CAST (a.units AS float) / CAST (b.bldgs AS int)),0))
-	,usb.improvement_value = (a.imps/b.bldgs)
+	usb.improvement_value = a.imps/b.bldgs
+	,usb.residential_sqft = CASE WHEN usb.development_type_id BETWEEN 19 AND 22
+								THEN a.sqft/b.bldgs
+								ELSE 0
+							END
+	,usb.non_residential_sqft = CASE WHEN usb.development_type_id NOT BETWEEN 19 AND 22
+								THEN a.sqft/b.bldgs
+								ELSE 0
+							END
+	,usb.year_built = a.year_built
+
 FROM
 	urbansim.buildings usb
 	LEFT JOIN 
@@ -91,16 +77,22 @@ FROM
 	 ON usb.parcel_id = b.parcel_id
 	LEFT JOIN
 		(SELECT l.parcelID
-			, par.[CURRENT_IMPS] imps
-			,l.units													--CHECK FIELD NAME
+			,LEFT(par.apn,8) apn8
+			,SUM([CURRENT_IMPS]) imps
+			,SUM([TOTAL_LVG_AREA]+[ADDITION_AREA]) sqft
+			,AVG(CASE WHEN [YEAR_EFFECTIVE] <= 16
+					THEN 2000 + CAST([YEAR_EFFECTIVE] AS smallint)
+					ELSE 1900 + CAST([YEAR_EFFECTIVE] AS smallint)
+				END ) year_built
+			,COUNT(*) num
 		FROM spacecore.input.assessor_par par
 		JOIN
 			(SELECT parcelID
 				,MIN(apn) apn											--GRAB LOWEST APN
-				,SUM(du) units
 			FROM spacecore.core.LANDCORE
 			GROUP BY parcelID) l
 		ON l.apn = LEFT(par.apn,8)										--ONE TO MANY, SELECT MIN APN
+		GROUP BY l.parcelID, LEFT(par.apn,8)
 		) a
 	ON usb.parcel_id = a.parcelID
 
@@ -109,10 +101,30 @@ FROM
 UPDATE
 	usb
 SET
-	--usb.residential_units = (c.number_of_units/b.bldgs)
-	usb.non_residential_sqft = (c.total_available_space/b.bldgs)
-	,usb.price_per_sqft = c.avg_asking_sf
-	,usb.year_built = c.year_built										--CHECK FIELD TYPE
+	usb.residential_sqft = CASE 
+								WHEN usb.development_type_id BETWEEN 19 AND 22 THEN 
+									CASE 
+										WHEN c.rentable_building_area > 0 THEN (c.rentable_building_area/b.bldgs)
+										ELSE usb.residential_sqft
+									END
+								ELSE usb.residential_sqft
+							END
+	,usb.non_residential_sqft = CASE 
+								WHEN usb.development_type_id NOT BETWEEN 19 AND 22 THEN 
+									CASE 
+										WHEN c.rentable_building_area > 0 THEN (c.rentable_building_area/b.bldgs)
+										ELSE usb.non_residential_sqft
+									END
+								ELSE usb.non_residential_sqft
+							END
+	,usb.year_built = CASE WHEN c.year_built > 0
+							THEN c.year_built
+							ELSE usb.year_built
+						END
+	,usb.stories = CASE WHEN c.stories > 0
+						THEN c.stories
+						ELSE 1
+					END	
 FROM
 	urbansim.buildings usb
 	LEFT JOIN 
@@ -120,12 +132,38 @@ FROM
 		FROM urbansim.buildings
 		GROUP BY parcel_id) b
 	 ON usb.parcel_id = b.parcel_id
-	LEFT JOIN
+	RIGHT JOIN
 		(SELECT parcel_id
-			--, SUM(number_of_units) number_of_units					--CHECK NULL VALUES
-			, SUM([total_available_space_(sf)]) total_available_space	--CHECK FIELD NAME
-			, MAX([avg_asking/sf]) avg_asking_sf						--CHECK FIELD NAME
-			, MIN(year_built) year_built								--CHECK FIELD NAME
+			, SUM([rentable_building_area]) rentable_building_area
+			, AVG([year_built]) year_built
+			, AVG([number_of_stories]) stories
 		FROM input.costar
 		GROUP BY parcel_id) c
 	ON usb.parcel_id = c.parcel_id
+
+
+/*
+<<<<<<< HEAD
+
+
+
+
+=======
+/** Use the COSTAR data to set the values for job_spaces **/
+/**
+TODO: There are missing records on both inner joins. 
+      parcel and sqft_per_job_by_devtype both need to
+      be updated.
+**/
+UPDATE
+    usb
+SET
+    --If the data is ***BAD(???)*** replace with 40 as the minimum
+    usb.job_spaces = CEILING(usb.non_residential_sqft / CAST(CASE WHEN ISNULL(sq.sqft_per_emp,0) < 40 THEN 40 ELSE sq.sqft_per_emp END as FLOAT))
+    ,usb.non_residential_rent_per_sqft = 0
+FROM
+    urbansim.buildings usb
+    INNER JOIN urbansim.parcels p ON usb.parcel_id = p.parcel_id
+    INNER JOIN urbansim.building_sqft_per_job sq ON usb.development_type_id = sq.development_type_id AND p.luz_id = sq.luz_id
+>>>>>>> ea548a1ef74e00dea6b16b8fadca53d50c152d20
+*/
