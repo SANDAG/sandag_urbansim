3e1f59451499e7182a756909556abef0b56243af
Elias Sanz <esa@sandag.org>
2016-05-03 10:33:12 -0700

    added square footage to suppport number of employees

--- etl/sql/views/buildings_nonres_synthetic.sql
+++ etl/sql/views/buildings_nonres_synthetic.sql
@@ -5,14 +5,16 @@ USE spacecore
 SELECT usp.parcel_id
 	,usp.luz_id
 	,usp.development_type_id
-	,ref.[development_type_id] devtype_from_lu
+	--,ref.[development_type_id] devtype_from_lu															--LCKEY LEVEL LU TO DEVTYPE
 --NOT GROUPED
-	,emp.emp_adj emp
-	,sqft.sqft_per_emp sqft_fromdev
-	,sqft_fromlu.sqft_per_emp sqft_fromlu
+	--,emp.emp_adj emp
+	--,sqft.sqft_per_emp sqft_fromdev
+	--,sqft_fromlu.sqft_per_emp sqft_fromlu																	--LCKEY LEVEL LU TO DEVTYPE
 --GROUPED
-	--,SUM(emp_adj) emp
-	--,MAX(sqft.sqft_per_emp) sqft
+	,SUM(emp_adj) emp
+	,MAX(sqft.sqft_per_emp) sqft
+--REQUIRED
+	,(SUM(emp_adj)*MAX(sqft.sqft_per_emp)) emp_sqft
 FROM urbansim.parcels usp
 	JOIN socioec_data.ca_edd.emp_2013 emp
 	ON usp.parcel_id = emp.parcelId		--emp.shape.STWithin(usp.shape) = 1								--EDD DATA SITED
@@ -20,11 +22,11 @@ FROM urbansim.parcels usp
 	ON emp.lu = ref.[lu_code]
 	JOIN [spacecore].[input].[sqft_per_emp_by_devType] sqft
 	ON usp.luz_id = sqft.luz_id AND usp.development_type_id = sqft.development_type_id 
-	JOIN [spacecore].[input].[sqft_per_emp_by_devType] sqft_fromlu
-	ON usp.luz_id = sqft_fromlu.luz_id AND ref.[development_type_id] = sqft_fromlu.development_type_id 
+	--JOIN [spacecore].[input].[sqft_per_emp_by_devType] sqft_fromlu
+	--ON usp.luz_id = sqft_fromlu.luz_id AND ref.[development_type_id] = sqft_fromlu.development_type_id	--LCKEY LEVEL LU TO DEVTYPE
 WHERE usp.parcel_id NOT IN (SELECT DISTINCT parcel_id FROM urbansim.buildings)							--NO BUILDING
-AND usp.development_type_id != ref.[development_type_id]
---GROUP BY usp.parcel_id, usp.luz_id, usp.development_type_id 
+--AND usp.development_type_id != ref.[development_type_id]													--PARCEL LEVEL DEVTYPE != LCKEY LEVEL LU TO DEVTYPE
+GROUP BY usp.parcel_id, usp.luz_id, usp.development_type_id 
 ORDER BY usp.parcel_id, usp.luz_id, usp.development_type_id
 
 
