a1b814b050cb6ace3be48d83f8af6c4b57a8a56e
Elias Sanz <esa@sandag.org>
2016-05-03 12:47:55 -0700

    generate table

--- etl/sql/views/buildings_nonres_synthetic.sql
+++ etl/sql/views/buildings_nonres_synthetic.sql
@@ -1,7 +1,28 @@
 USE spacecore
+IF OBJECT_ID('urbansim.buildings_nonres_synthetic') IS NOT NULL
+    DROP TABLE urbansim.buildings_nonres_synthetic
+GO
 
+CREATE TABLE urbansim.buildings_nonres_synthetic(
+	building_id int IDENTITY(1,1) NOT NULL
+	,parcel_id int
+	,luz_id int
+	,development_type_id smallint
+	,emp int
+	,sqft float
+	,emp_sqft float
+	,apn8 int
+	,par_nrsf float
+	,costar_nrsf float
+)
 /**1 IDENTIFY PARCELS WHERE EDD DATA HAS BEEN SITED, BUT NO BUILDING EXISTS IN OUR DATABASE **/
-/**2 USING THE SQUARE FOOT PER EMPLOYEE DATA, DETERMINE THE NON-RESIDENTIAL SQUARE FOOTAGE NECESSARY TO SUPPORT THE EDD NUMBER OF EMPLOYEES **/
+INSERT INTO urbansim.buildings_nonres_synthetic WITH (TABLOCK) (
+	parcel_id
+	,luz_id
+	,development_type_id
+	,emp
+
+)
 SELECT usp.parcel_id
 	,usp.luz_id
 	,usp.development_type_id
@@ -11,25 +32,34 @@ SELECT usp.parcel_id
 	--,sqft.sqft_per_emp sqft_fromdev
 	--,sqft_fromlu.sqft_per_emp sqft_fromlu																	--LCKEY LEVEL LU TO DEVTYPE
 --GROUPED
-	,SUM(emp_adj) emp
-	,MAX(sqft.sqft_per_emp) sqft
---REQUIRED
-	,(SUM(emp_adj)*MAX(sqft.sqft_per_emp)) emp_sqft
+	,SUM(emp.emp_adj) emp
 FROM urbansim.parcels usp
 	JOIN socioec_data.ca_edd.emp_2013 emp
 	ON usp.parcel_id = emp.parcelId		--emp.shape.STWithin(usp.shape) = 1								--EDD DATA SITED
-	JOIN [ref].[development_type_lu_code] ref
-	ON emp.lu = ref.[lu_code]
-	JOIN [spacecore].[input].[sqft_per_emp_by_devType] sqft
-	ON usp.luz_id = sqft.luz_id AND usp.development_type_id = sqft.development_type_id 
 	--JOIN [spacecore].[input].[sqft_per_emp_by_devType] sqft_fromlu
 	--ON usp.luz_id = sqft_fromlu.luz_id AND ref.[development_type_id] = sqft_fromlu.development_type_id	--LCKEY LEVEL LU TO DEVTYPE
 WHERE usp.parcel_id NOT IN (SELECT DISTINCT parcel_id FROM urbansim.buildings)							--NO BUILDING
 --AND usp.development_type_id != ref.[development_type_id]													--PARCEL LEVEL DEVTYPE != LCKEY LEVEL LU TO DEVTYPE
 GROUP BY usp.parcel_id, usp.luz_id, usp.development_type_id 
 ORDER BY usp.parcel_id, usp.luz_id, usp.development_type_id
+;
 
+/**2 USING THE SQUARE FOOT PER EMPLOYEE DATA, DETERMINE THE NON-RESIDENTIAL SQUARE FOOTAGE NECESSARY TO SUPPORT THE EDD NUMBER OF EMPLOYEES **/
+UPDATE
+	usbs
+SET
+	usbs.sqft = sqft.sqft_per_emp
+	,usbs.emp_sqft = usbs.emp * sqft.sqft_per_emp		--REQUIRED SQFT
+FROM
+	urbansim.buildings_nonres_synthetic usbs
+JOIN socioec_data.ca_edd.emp_2013 emp
+ON usbs.parcel_id = emp.parcelId
 
+--JOIN [ref].[development_type_lu_code] ref																	--LCKEY LEVEL LU TO DEVTYPE
+--ON emp.lu = ref.[lu_code]
+
+JOIN [spacecore].[input].[sqft_per_emp_by_devType] sqft
+ON usbs.luz_id = sqft.luz_id AND usbs.development_type_id = sqft.development_type_id 	
 
 /**3 USE PAR / COSTAR DATA, IF AVAILABLE, TO VALIDATE DERIVED NON-RESIDENTIAL SQUARE FOOTAGE **/
 
