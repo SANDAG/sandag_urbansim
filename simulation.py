e5b3306b5d3503a55a63398895720519f4510945
Clint Daniels <clint.daniels@sandag.org>
2016-05-11 09:47:16 -0700

    Add residential_units to the building export

--- simulation.py
+++ simulation.py
@@ -10,7 +10,7 @@ orca.run([#'scheduled_development_events'
 ], iter_vars=range(2015,2016))
 
 orca.get_table('nodes').to_frame().to_csv('data/nodes.csv')
-orca.get_table('buildings').to_frame(['building_id','parcel_id','building_type_id','residential_price','non_residential_price','distance_to_park','distance_to_school','year_built']).to_csv('data/buildings.csv')
+orca.get_table('buildings').to_frame(['building_id','parcel_id','building_type_id','residential_units','residential_price','non_residential_price','distance_to_park','distance_to_school','year_built']).to_csv('data/buildings.csv')
 orca.get_table('households').to_frame(['household_id', 'building_id', 'persons', 'age_of_head', 'income', 'income_quartile']).to_csv('data/households.csv')
 orca.get_table('jobs').to_frame(['job_id', 'building_id', 'sector_id']).to_csv('data/jobs.csv')
 orca.get_table('feasibility').to_frame().to_csv('data/feasibility.csv')
