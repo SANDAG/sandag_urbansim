3c16e4ea0df44556fe65f2cd96f50db4b4533269
Elias Sanz <esa@sandag.org>
2016-06-13 10:45:57 -0700

    updated to pysandag tools

--- etl/postgresql/parcels_load.py
+++ etl/postgresql/parcels_load.py
@@ -1,7 +1,9 @@
 import geoalchemy2
 import pandas as pd
 import sqlalchemy
-from util import get_connection_string, transform_wkt
+##from util import get_connection_string, transform_wkt
+from pysandag.database import get_connection_string
+from pysandag.gis import transform_wkt
 from sqlalchemy import create_engine
 
 #GET THE CONNECTION STRINGS
