1151d66cbf5f2242923839cafdec3d23bba8bf29
Anne Kuller <aku@sandag.org>
2016-07-05 11:24:18 -0700

    simulation for residential only

--- simulation.py
+++ simulation.py
@@ -1,21 +1,33 @@
 import orca
 import models, datasources, variables
+import sys
+
+# orig_stdout = sys.stdout
+# f = file('data\\stdout.txt', 'w')
+# sys.stdout = f
 
 orca.run(['build_networks'])
 
-orca.run([#'scheduled_development_events'
-         'neighborhood_vars','rsh_simulate','nrh_simulate','nrh_simulate2'
-         ,'jobs_transition',"elcm_simulate"
-         ,'households_transition', "hlcm_simulate"
-         ,"price_vars","feasibility","residential_developer","non_residential_developer"]
-         ,iter_vars=range(2015,2016))
+# residential only
+orca.run([#'scheduled_development_events',
+          'neighborhood_vars',
+          'rsh_simulate',
+          'households_transition',
+          "hlcm_simulate",
+          "price_vars",
+          "feasibility",
+          "residential_developer"
+          ], iter_vars=range(2015,2050),data_out='data\\results.h5', out_interval=1)
 
 orca.get_table('nodes').to_frame().to_csv('data/nodes.csv')
 orca.get_table('buildings').to_frame().to_csv('data/buildings.csv')
 orca.get_table('households').to_frame().to_csv('data/households.csv')
-orca.get_table('jobs').to_frame().to_csv('data/jobs.csv')
+# orca.get_table('jobs').to_frame().to_csv('data/jobs.csv')
 orca.get_table('feasibility').to_frame().to_csv('data/feasibility.csv')
 orca.get_table('parcels').to_frame().to_csv('data/parcels.csv')
+
+# sys.stdout = orig_stdout
+# f.close()
 
 
 
