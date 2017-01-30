335275070f70139943fc509ea9c4368eca293de2
SANDAGNET\rco <rachel.cortes@sandag.org>
2017-01-30 15:31:35 -0800

    check number of du in the buildings_res_units_field versus ludu2015 on lis.gis via query on sql server management studio

---
+++ etl/postgresql/QC check/check_du_in_buildings_file_vs_ludu2015.sql
@@ -0,0 +1,5 @@
+﻿SELECT jurisdiction_id, 
+       sum(buildings_res_units) buildings_res_units 
+FROM urbansim_output.res_capacity_ludu2015_to_sr13
+GROUP BY jurisdiction_id 
+ORDER BY jurisdiction_id  
