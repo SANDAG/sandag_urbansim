696465e27a5c6dc52ceeac89d0665b57d185a11e
Elias Sanz <esa@sandag.org>
2017-01-31 18:13:47 -0800

    revise updated cap_hs , updated column names

--- etl/sql/sr13capacity_to_ludu2015.sql
+++ etl/sql/sr13capacity_to_ludu2015.sql
@@ -123,20 +123,21 @@ WHERE subParcel IS NULL
 
 /*** CREATE TABLE ref.sr13_capacity ***/
 CREATE TABLE ref.sr13capacity_ludu2015(
-	parcel_id_ludu2015 int NOT NULL
-	,du_sr13 int NULL
-	,cap_hs_sr13 int NULL
-	,du_ludu2015 int NULL
-	,cap_hs_ludu2015 int NULL
+	ludu2015_parcel_id int NOT NULL
+	,sr13_du int NULL
+	,sr13_cap_hs_with_negatives int NULL
+	,ludu2015_du int NULL
+	,sr13_cap_hs_growth_adjusted int NULL
 	,update_2015 varchar(32)
+	,revised varchar(1)
 ) 
 GO
 
 --LOAD LCKey TO parcel FOR RECORDS FOUND IN ludu2015
 INSERT INTO ref.sr13capacity_ludu2015(
-	parcel_id_ludu2015
-	,du_sr13
-	,cap_hs_sr13
+	ludu2015_parcel_id
+	,sr13_du
+	,sr13_cap_hs_with_negatives
 	,update_2015)
 SELECT
 	parcelId
@@ -161,24 +162,24 @@ WITH sr13 AS(
 	FROM [staging].[sr13_missing_match] AS sr13
 	LEFT JOIN [GIS].[ludu2015] AS ludu2015
 		ON sr13.subParcel = ludu2015.subParcel
-	WHERE ludu2015.parcelID IN (SELECT parcel_id_ludu2015 FROM ref.sr13capacity_ludu2015)
+	WHERE ludu2015.parcelID IN (SELECT ludu2015_parcel_id FROM ref.sr13capacity_ludu2015)
 	GROUP BY ludu2015.parcelID
-	ORDER BY 3 DESC
+	--ORDER BY 3 DESC
 )
 UPDATE sr_ludu
 SET
-	du_sr13 = du_sr13 + sr13.du
-	,cap_hs_sr13 = cap_hs_sr13 + sr13.cap_hs
+	sr13_du = sr13_du + sr13.du
+	,sr13_cap_hs_with_negatives = sr13_cap_hs_with_negatives + sr13.cap_hs
 	,update_2015 = 'parcel_and_centroid_to_parcel'
 FROM ref.sr13capacity_ludu2015 AS sr_ludu
-JOIN sr13 ON sr_ludu.parcel_id_ludu2015 = sr13.parcelID
+JOIN sr13 ON sr_ludu.ludu2015_parcel_id = sr13.parcelID
 
 
 --LOAD LCKey TO parcel FOR RECORDS NOT FOUND IN ludu2015 AND NOT ALREADY ADDED
 INSERT INTO ref.sr13capacity_ludu2015(
-	parcel_id_ludu2015
-	,du_sr13
-	,cap_hs_sr13
+	ludu2015_parcel_id
+	,sr13_du
+	,sr13_cap_hs_with_negatives
 	,update_2015)
 SELECT
 	ludu2015.parcelID
@@ -188,7 +189,7 @@ SELECT
 FROM [staging].[sr13_missing_match] AS sr13
 LEFT JOIN [GIS].[ludu2015] AS ludu2015
 	ON sr13.subParcel = ludu2015.subParcel
-WHERE ludu2015.parcelID NOT IN (SELECT parcel_id_ludu2015 FROM ref.sr13capacity_ludu2015)
+WHERE ludu2015.parcelID NOT IN (SELECT ludu2015_parcel_id FROM ref.sr13capacity_ludu2015)
 GROUP BY ludu2015.parcelID
 
 
@@ -205,7 +206,7 @@ JOIN (SELECT *
 	ON ludu.LCKey = cap.LCKey
 
 --FROM sr13capacity_ludu2015
-SELECT COUNT(*), SUM(du_sr13), SUM(cap_hs_sr13) FROM ref.sr13capacity_ludu2015 
+SELECT COUNT(*), SUM(sr13_du), SUM(sr13_cap_hs_growth_adjusted) FROM ref.sr13capacity_ludu2015 
 
 
 --SELECT update_2015, COUNT(*) FROM ref.sr13capacity_ludu2015 GROUP BY update_2015
@@ -213,44 +214,142 @@ SELECT COUNT(*), SUM(du_sr13), SUM(cap_hs_sr13) FROM ref.sr13capacity_ludu2015
 --UPDATE ludu2015 du
 UPDATE sr_ludu
 SET
-	du_ludu2015 = ludu2015.du
+	ludu2015_du = ludu2015.du
 FROM ref.sr13capacity_ludu2015 AS sr_ludu
 JOIN (SELECT parcelID, SUM(du) AS du FROM GIS.ludu2015 GROUP BY parcelID) AS ludu2015
-ON sr_ludu.parcel_id_ludu2015 = ludu2015.parcelID
+ON sr_ludu.ludu2015_parcel_id = ludu2015.parcelID
 
 
 --CALCULATE UPDATED cap_hs TO 2015
 SELECT
-	parcel_id_ludu2015
-	,du_sr13
-	,du_ludu2015
-	,(du_ludu2015 - du_sr13) AS growth
-	,cap_hs_sr13
+	ludu2015_parcel_id
+	,sr13_du
+	,ludu2015_du
+	,(ludu2015_du - sr13_du) AS growth
+	,sr13_cap_hs_with_negatives
 	,CASE
-		WHEN (du_ludu2015 = du_sr13) THEN cap_hs_sr13		--NO CHANGE IN du, cap_hs REMAINS
-		WHEN (du_ludu2015 - du_sr13 = cap_hs_sr13) THEN 0	--GROWTH MATCHES cap_hs
-		WHEN (du_ludu2015 - du_sr13 > cap_hs_sr13) THEN 0	--GROWTH IS GREATER THAN EXPECTED cap_hs (NEGATIVE UPDATED cap_hs)
-		WHEN (du_ludu2015 < du_sr13) THEN cap_hs_sr13		--FOR NEGATIVE GROWTH KEEP cap_hs
-		ELSE cap_hs_sr13 - (du_ludu2015 - du_sr13)			--CALCULATE GROWTH, SUBSTRACT FROM cap_hs
+		WHEN (ludu2015_du = sr13_du) THEN sr13_cap_hs_with_negatives		--NO CHANGE IN du, cap_hs REMAINS
+		WHEN (ludu2015_du - sr13_du = sr13_cap_hs_with_negatives) THEN 0	--GROWTH MATCHES cap_hs
+		WHEN (ludu2015_du - sr13_du > sr13_cap_hs_with_negatives) THEN 0	--GROWTH IS GREATER THAN EXPECTED cap_hs (NEGATIVE UPDATED cap_hs)
+		WHEN (ludu2015_du < sr13_du) THEN sr13_cap_hs_with_negatives		--FOR NEGATIVE GROWTH KEEP cap_hs
+		ELSE sr13_cap_hs_with_negatives - (ludu2015_du - sr13_du)			--CALCULATE GROWTH, SUBSTRACT FROM cap_hs
 	END AS cap_hs_to2015
 	,update_2015
 FROM ref.sr13capacity_ludu2015
---WHERE du_sr13 <> du_ludu2015	--NO CHANGE IN du
+--WHERE sr13_du <> ludu2015_du	--NO CHANGE IN du
 ORDER BY 4 DESC
 
 
 --UPDATE cap_hs TO 2015
 UPDATE ref.sr13capacity_ludu2015
 SET
-	cap_hs_ludu2015 = CASE
-						WHEN (du_ludu2015 = du_sr13) THEN cap_hs_sr13		--NO CHANGE IN du, cap_hs REMAINS
-						WHEN (du_ludu2015 - du_sr13 = cap_hs_sr13) THEN 0	--GROWTH MATCHES cap_hs
-						WHEN (du_ludu2015 - du_sr13 > cap_hs_sr13) THEN 0	--GROWTH IS GREATER THAN EXPECTED cap_hs (NEGATIVE UPDATED cap_hs)
-						WHEN (du_ludu2015 < du_sr13) THEN cap_hs_sr13		--FOR NEGATIVE GROWTH KEEP cap_hs
-						ELSE cap_hs_sr13 - (du_ludu2015 - du_sr13)			--CALCULATE GROWTH, SUBSTRACT FROM cap_hs
+	sr13_cap_hs_growth_adjusted = CASE
+						WHEN (ludu2015_du = sr13_du) THEN sr13_cap_hs_with_negatives		--NO CHANGE IN du, cap_hs REMAINS
+						WHEN (ludu2015_du - sr13_du = sr13_cap_hs_with_negatives) THEN 0	--GROWTH MATCHES cap_hs
+						WHEN (ludu2015_du - sr13_du > sr13_cap_hs_with_negatives) THEN 0	--GROWTH IS GREATER THAN EXPECTED cap_hs (NEGATIVE UPDATED cap_hs)
+						WHEN (ludu2015_du < sr13_du) THEN sr13_cap_hs_with_negatives		--FOR NEGATIVE GROWTH KEEP cap_hs
+						ELSE sr13_cap_hs_with_negatives - (ludu2015_du - sr13_du)			--CALCULATE GROWTH, SUBSTRACT FROM cap_hs
 					END
 
 
+-- REVISE PARCELS WHERE cap_hs CHANGED
+SELECT * FROM ref.sr13capacity_ludu2015
+WHERE sr13_cap_hs_with_negatives <> sr13_cap_hs_growth_adjusted
+ORDER BY sr13_cap_hs_growth_adjusted DESC
+
+
+--REVISE PARCELS WITH LARGE cap_hs BY CITY
+WITH c AS(
+	SELECT * FROM ref.sr13capacity_ludu2015 AS c
+	JOIN (SELECT parcelID, mgra FROM gis.ludu2015) AS m
+	ON c.ludu2015_parcel_id = m.parcelID
+	WHERE sr13_cap_hs_with_negatives <> sr13_cap_hs_growth_adjusted
+)
+SELECT
+	c.*
+	,m.city
+FROM c
+JOIN OPENQUERY(sql2014b8, 'SELECT * FROM [lis].[gis].[MGRA13]') AS m
+	ON c.mgra = m.mgra
+WHERE m.city IN(6, 16)
+ORDER BY c.sr13_cap_hs_with_negatives DESC
+
+
+/*** ADJUST ***/
+--TO ADJUST cap_hs TO 0, CREATE .csv FILE WITH parcel_id
+CREATE TABLE staging.sr13_cap_adjust (
+parcel_id int NOT NULL
+);
+
+--ADD parcel_id THAT ARE NOT IN ORIGINAL CAPACITY, INTO .csv
+
+--ADJUST [sr13_cap_hs_growth_adjusted] AND TAKE NOTE [adjusted_cap_hs]
+BULK INSERT staging.sr13_cap_adjust
+FROM '\\nasb8\Shared\TEMP\esa\cap_hs\cap_hs_sr13_to_ludu15_adjust.csv'	--NETWORK
+WITH
+(
+	FIRSTROW = 1,
+	FIELDTERMINATOR = ',',  --CSV field delimiter
+	ROWTERMINATOR = '\n',   --Use to shift the control to next row
+	TABLOCK
+)
+
+--JOIN TABLES FOR ADDITIONAL parcel_id
+SELECT
+	ludu2015_parcel_id
+	,parcel_id
+	,COALESCE(ludu2015_parcel_id, parcel_id) AS parcel_id_all
+	,sr13_cap_hs_growth_adjusted
+	,CASE
+		WHEN parcel_id IS NOT NULL THEN 0
+		ELSE sr13_cap_hs_growth_adjusted
+	END AS cap_hs_adjusted
+FROM ref.sr13capacity_ludu2015 AS c
+FULL OUTER JOIN staging.sr13_cap_adjust AS a
+ON c.ludu2015_parcel_id = a.parcel_id
+--WHERE a.parcel_id IS NOT NULL	--JUST FROM UPDATE TABLE
+ORDER BY 2 DESC
+
+--INSERT NEW parcel_id RECORDS
+INSERT INTO ref.sr13capacity_ludu2015(
+	ludu2015_parcel_id
+	,sr13_cap_hs_growth_adjusted
+	,revised)
+SELECT parcel_id
+	,0
+	,'Y'
+FROM staging.sr13_cap_adjust
+WHERE parcel_id NOT IN (SELECT ludu2015_parcel_id FROM ref.sr13capacity_ludu2015)	--NOT ALREADY IN TABLE
+ORDER BY parcel_id
+
+
+--UPDATE [sr13_cap_hs_growth_adjusted]
+UPDATE c
+SET
+	[ludu2015_parcel_id] = COALESCE(c.ludu2015_parcel_id, a.parcel_id)
+	,[sr13_cap_hs_growth_adjusted] = CASE 
+		WHEN parcel_id IS NOT NULL THEN 0
+		ELSE sr13_cap_hs_growth_adjusted
+		END
+	,[revised] = 'Y'
+FROM ref.sr13capacity_ludu2015 AS c
+FULL OUTER JOIN staging.sr13_cap_adjust AS a
+ON c.ludu2015_parcel_id = a.parcel_id
+WHERE a.parcel_id IS NOT NULL	--DO FOR UPDATE TABLE
+
+
+SELECT SUM([sr13_cap_hs_growth_adjusted])	--383,235
+FROM [ref].[sr13capacity_ludu2015]
+
+
+/* CHECKS */
+SELECT 
+	SUM(sr13_du) AS sr13_du
+	,SUM(sr13_cap_hs_with_negatives) AS sr13_cap_hs_with_negatives
+	,SUM(ludu2015_du) AS ludu2015_du
+	,SUM(sr13_cap_hs_growth_adjusted) AS sr13_cap_hs_growth_adjusted
+FROM ref.sr13capacity_ludu2015
 
 SELECT * FROM ref.sr13capacity_ludu2015
-WHERE du_sr13 <> du_ludu2015
+WHERE sr13_du <> ludu2015_du
+
