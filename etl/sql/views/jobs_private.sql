7e256bfb1bc51030a2293f16a180d3c47a1ea98d
Elias Sanz <esa@sandag.org>
2017-07-28 15:43:14 -0700

    job allocation loops by sectors, increased preformance

--- etl/sql/views/jobs_private.sql
+++ etl/sql/views/jobs_private.sql
@@ -81,125 +81,145 @@ FULL OUTER JOIN jobs
 ORDER BY COALESCE(jobs.sector_id, job_spaces.sector_id)
 --*/
 
+
 /*##### STEP 1 - CALCULATE DISTANCES INTO STAGING TABLE #####*/
-DECLARE @radius		int = 1320;	--1/4 MILE SEARCH RADIUS START
-DECLARE @radius_i	int = 1320;	--1/4 MILE SEARCH RADIUS, INCREMENTS
-DECLARE @run smallint = 1;	--NUMBERED RUNS TO TRACK ALLOCATION
+DECLARE @sector_id	smallint = 5;	--SECTOR ID
 
---** WHILE LOOP START>> *********************************************************************************************************************
-WHILE (
-	--CHECK FOR REMAINING JOBS
-	SELECT COUNT(*)
-	FROM input.jobs_wac_2012_2016
-	WHERE yr = 2015
-	AND job_id NOT IN(SELECT job_id FROM urbansim.jobs)
-	) > 0
+--** WHILE LOOP START>> ******************************************************************
+WHILE (@sector_id <= 20)
 BEGIN
+			
+	DECLARE @radius		int = 1320;		--1/4 MILE SEARCH RADIUS START
+	DECLARE @radius_i	int = 1320;		--1/4 MILE SEARCH RADIUS, INCREMENTS
+	DECLARE @run		smallint = 1;	--NUMBERED RUNS TO TRACK ALLOCATION
+	
+	--** WHILE LOOP START>> ******************************************************************
+	WHILE (
+		--CHECK FOR REMAINING JOBS
+		SELECT COUNT(*)
+		FROM input.jobs_wac_2012_2016
+		WHERE yr = 2015
+		AND job_id NOT IN(SELECT job_id FROM urbansim.jobs)
+		AND sector_id = @sector_id
+		) > 0
+	BEGIN
 
-	DROP TABLE staging.near;
+		--DROP TABLE #spaces
+		--DROP TABLE #jobs
+		--DROP TABLE #near
 
-	WITH spaces AS(
+		--SPACES #TABLE
 		SELECT
 			js.building_id
 			,js.sector_id
 			,js.job_spaces - COALESCE(jsu.job_spaces_used, 0) AS job_spaces
 			,usb.shape
+		INTO #spaces
 		FROM urbansim.job_spaces AS js
 		LEFT JOIN(SELECT building_id
-						,sector_id
 						,COUNT(building_id) AS job_spaces_used
 					FROM urbansim.jobs
-					GROUP BY building_id
-						,sector_id) AS jsu
+					WHERE sector_id = @sector_id
+					GROUP BY building_id) AS jsu
 			ON js.building_id = jsu.building_id
-			AND js.sector_id = jsu.sector_id
 		JOIN urbansim.buildings AS usb ON js.building_id = usb.building_id
 		WHERE js.job_spaces - COALESCE(jsu.job_spaces_used, 0) > 0
-		--ORDER BY building_id, sector_id--
-	)
-	, jobs AS(
+		AND js.sector_id = @sector_id
+		--CREATE SPATIAL INDEX
+		ALTER TABLE #spaces ALTER COLUMN building_id bigint NOT NULL
+		ALTER TABLE #spaces ADD CONSTRAINT pk_spaces_building_id PRIMARY KEY CLUSTERED (building_id) 
+		ALTER TABLE #spaces ALTER COLUMN shape geometry NOT NULL
+		CREATE SPATIAL INDEX [ix_spatial_spaces_shape] ON #spaces
+		(
+			shape
+		) USING  GEOMETRY_GRID
+			WITH (BOUNDING_BOX =(6152300, 1775400, 6613100, 2129400), GRIDS =(LEVEL_1 = MEDIUM,LEVEL_2 = MEDIUM,LEVEL_3 = MEDIUM,LEVEL_4 = MEDIUM), 
+			CELLS_PER_OBJECT = 16, PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+		;
+
+		--JOBS #TABLE
 		SELECT 
 			job_id
 			,wac.block_id
 			,sector_id
-			,b.cent
+			,Shape.STCentroid() AS cent
+			,Shape.STCentroid().STBuffer(@radius) AS buff
+		INTO #jobs
 		FROM input.jobs_wac_2012_2016 AS wac
-		JOIN (SELECT BLOCKID10 AS block_id, Shape.STCentroid() AS cent FROM ref.blocks) AS b ON wac.block_id = b.block_id 
+		JOIN (SELECT BLOCKID10 AS block_id, Shape FROM ref.blocks) AS b ON wac.block_id = b.block_id 
 		WHERE yr = 2015
 		AND job_id NOT IN(SELECT job_id FROM urbansim.jobs)
-	)
-	, distances AS(				--**DO FOR DISTANCE INCREMENTS OF 1/4 MILE (1,320, 2,640, 3,960... ft)
-		SELECT
-			building_id
-			,b.block_id
-			,CAST(b.cent.STDistance(usb.shape) AS int) AS dist
-		FROM (SELECT *
-				FROM urbansim.buildings
-				WHERE building_id IN (	SELECT DISTINCT js.building_id
-												--js.building_id, js.sector_id, job_spaces, job_spaces_used--
-										FROM urbansim.job_spaces AS js
-										JOIN (SELECT building_id				--JOB SPACES USED
-													,sector_id
-													,COUNT(building_id) AS job_spaces_used
-												FROM urbansim.jobs
-												GROUP BY building_id
-													,sector_id) AS jsu
-										ON js.building_id = jsu.building_id
-										AND js.sector_id = jsu.sector_id
-										WHERE job_spaces > job_spaces_used
-										--ORDER BY building_id--
-										)
-			)AS usb
-		JOIN (SELECT BLOCKID10 AS block_id, Shape.STCentroid() AS cent FROM ref.blocks) AS b
-			ON b.cent.STBuffer(@radius).STIntersects(usb.shape) = 1
-	)
+		AND sector_id = @sector_id
+		--CREATE SPATIAL INDEX
+		ALTER TABLE #jobs ALTER COLUMN job_id int NOT NULL
+		ALTER TABLE #jobs ADD CONSTRAINT pk_jobs_job_id PRIMARY KEY CLUSTERED (job_id) 
+		ALTER TABLE #jobs ALTER COLUMN cent geometry NOT NULL
+		CREATE SPATIAL INDEX [ix_spatial_spaces_cent] ON #jobs
+		(
+			cent
+		) USING  GEOMETRY_GRID
+			WITH (BOUNDING_BOX =(6152300, 1775400, 6613100, 2129400), GRIDS =(LEVEL_1 = MEDIUM,LEVEL_2 = MEDIUM,LEVEL_3 = MEDIUM,LEVEL_4 = MEDIUM), 
+			CELLS_PER_OBJECT = 16, PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+		ALTER TABLE #jobs ALTER COLUMN buff geometry NOT NULL
+		CREATE SPATIAL INDEX [ix_spatial_spaces_buff] ON #jobs
+		(
+			buff
+		) USING  GEOMETRY_GRID
+			WITH (BOUNDING_BOX =(6152300, 1775400, 6613100, 2129400), GRIDS =(LEVEL_1 = MEDIUM,LEVEL_2 = MEDIUM,LEVEL_3 = MEDIUM,LEVEL_4 = MEDIUM), 
+			CELLS_PER_OBJECT = 16, PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+		;
+
+		PRINT 'JOBS AND SPACES READY'
+		--FIND NEAR FEATURES AND STORE TO TABLE
 		SELECT
 			jobs.job_id
 			,jobs.sector_id
 			,spaces.building_id
 			,spaces.job_spaces
-			,distances.dist
-		INTO staging.near								--**SELECT INTO
-		FROM jobs
-		JOIN spaces
-			ON jobs.cent.STBuffer(@radius).STIntersects(spaces.shape) = 1
-			AND jobs.sector_id = spaces.sector_id
-		JOIN distances
-			ON jobs.block_id = distances.block_id
-			AND spaces.building_id = distances.building_id
+			,CAST(jobs.cent.STDistance(spaces.shape) AS int) AS dist
+		INTO #near																		--**SELECT INTO
+		FROM #jobs AS jobs
+			,#spaces AS spaces
+		WHERE jobs.buff.STIntersects(spaces.shape) = 1
+		PRINT 'DISTANCES CALCULATED'
 
+		DROP TABLE #spaces	--SELECT * FROM #spaces
+		DROP TABLE #jobs	--SELECT * FROM #jobs
 
-	PRINT @radius
-	PRINT @run
+		PRINT @radius
+		PRINT @run
+
 
 		/*##### STEP 2 - ALLOCATE JOBS BY DISTANCE #####*/
-		--** WHILE LOOP START>> *********************************************************************************************************************
+		--** WHILE LOOP START>> ******************************************************************
 		--START ALLOCATION
-		WHILE (SELECT SUM(job_spaces) FROM staging.near) > 0					--ITERATE- RUN MULTIPLE TIMES FOR EACH DISTANCE, UNTIL ALLOCATION IS COMPLETE
+		WHILE (SELECT SUM(job_spaces) FROM #near) > 0					--ITERATE- RUN MULTIPLE TIMES FOR EACH DISTANCE, UNTIL ALLOCATION IS COMPLETE
 		BEGIN
-			WITH nearest AS(	
-				SELECT
-					ROW_NUMBER() OVER(PARTITION BY job_id, sector_id ORDER BY job_id, dist) dist_id
-					,job_id
-					,sector_id
-					,building_id
-					,job_spaces
-					,dist
-				FROM staging.near
-				WHERE job_spaces > 0
-			)
-			,grab AS(	
-				SELECT
-					ROW_NUMBER() OVER(PARTITION BY building_id, sector_id ORDER BY dist) row_id
-					,job_id
-					,sector_id
-					,building_id
-					,job_spaces
-					--,dist--
-				FROM nearest
-				WHERE dist_id = 1
-				--ORDER BY building_id--
-			)
+
+			--NEAREST #TABLE
+			SELECT
+				ROW_NUMBER() OVER(PARTITION BY job_id ORDER BY job_id, dist) dist_id
+				,job_id
+				,sector_id
+				,building_id
+				,job_spaces
+				,dist
+			INTO #nearest
+			FROM #near
+			WHERE job_spaces > 0
+
+			--GRAB #TABLE	
+			SELECT
+				ROW_NUMBER() OVER(PARTITION BY building_id ORDER BY dist) row_id
+				,job_id
+				,sector_id
+				,building_id
+				,job_spaces
+			INTO #grab
+			FROM #nearest
+			WHERE dist_id = 1
+
+			--ALLOCATE JOBS AND STORE TO TABLE
 			INSERT INTO urbansim.jobs (job_id, sector_id, building_id, source, run)			--**SELECT INTO
 			SELECT 
 				job_id
@@ -207,47 +227,56 @@ BEGIN
 				,building_id
 				,'WAC'
 				,@run																		--**INSERT RUN NUMBER
-			FROM grab
+			FROM #grab
 			WHERE row_id <= job_spaces
-			ORDER BY building_id, sector_id--
 			;
 
+			DROP TABLE #nearest
+			DROP TABLE #grab
+
 			--UPDATE JOBS LIST
-			DELETE FROM staging.near WHERE job_id IN (SELECT job_id FROM urbansim.jobs)
+			DELETE FROM #near WHERE job_id IN (SELECT job_id FROM urbansim.jobs WHERE run > 0)
 			;
 
 			--UPDATE JOB SPACES
 			UPDATE n
 			SET job_spaces = js.job_spaces_old - jsu.job_spaces_used
-			FROM staging.near AS n
-			JOIN(SELECT building_id, sector_id
+			FROM #near AS n
+			JOIN(SELECT building_id
 							,COUNT(building_id) AS job_spaces_used
 						FROM urbansim.jobs
-						GROUP BY building_id, sector_id) AS jsu
+						WHERE sector_id = @sector_id
+						GROUP BY building_id) AS jsu
 				ON n.building_id = jsu.building_id
-				AND n.sector_id = jsu.sector_id
-			JOIN(SELECT building_id, sector_id
+			JOIN(SELECT building_id
 							,job_spaces AS job_spaces_old
-						FROM urbansim.job_spaces) AS js
+						FROM urbansim.job_spaces
+						WHERE sector_id = @sector_id
+						) AS js
 				ON n.building_id = js.building_id
-				AND n.sector_id = js.sector_id
 			PRINT 'ANOTHER LOOP FINISHED'
 
-			IF (SELECT COUNT(*) FROM staging.near) = 0
+			IF (SELECT COUNT(*) FROM #near) = 0
 			BEGIN
+				DROP TABLE #near
 				BREAK
 				PRINT 'ALL JOBS ALLOCATED'
-			END
-
-		END
+			END;
+	
+		END;
 		PRINT 'OUT OF JOB SPACES'
 		;
-		--** WHILE LOOP END << *********************************************************************************************************************
+		--** WHILE LOOP END << ******************************************************************
+	DROP TABLE #near
 	SET @radius = @radius + @radius_i
 	SET @run = @run + 1
 
+	END;
+	--** WHILE LOOP END << ******************************************************************
+
+SET @sector_id = @sector_id + 1
 END;
---** WHILE LOOP END << *********************************************************************************************************************
+--** WHILE LOOP END << ******************************************************************
 
 --CHECK FOR REMAINING JOBS
 SELECT COUNT(*)
@@ -255,4 +284,5 @@ FROM input.jobs_wac_2012_2016
 WHERE yr = 2015
 AND job_id NOT IN(SELECT job_id FROM urbansim.jobs)
 ;
+
 
