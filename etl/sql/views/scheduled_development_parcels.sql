6aa1b2d68f1ec644a0a4b96f521d28b254d6894c
Elias Sanz <esa@sandag.org>
2018-04-25 13:05:13 -0700

    added 2 site overrides, GQ fix

--- etl/sql/views/scheduled_development_parcels.sql
+++ etl/sql/views/scheduled_development_parcels.sql
@@ -52,3 +52,135 @@ CREATE SPATIAL INDEX ix_spatial_urbansim_scheddevparcels_shape ON urbansim.sched
 ;
 
 
+/****** FIX SITE 15016 ******/
+INSERT INTO urbansim.scheduled_development_parcels
+SELECT
+	site_id
+	,parcel_id
+	,0
+	,0
+	,0
+	,0
+	,'use scheddevassign no res unit'
+	,'cchu'
+	,shape
+FROM urbansim.parcels AS usp
+WHERE site_id IN (15005, 15016)
+AND parcel_id NOT IN
+	(SELECT parcel_id 
+	FROM urbansim.scheduled_development_parcels AS sdp 
+	WHERE site_id IN (15005, 15016))
+ORDER BY
+	site_id
+	,parcel_id
+;
+
+ 
+/****** INSERT GQ ******/
+--LOAD FROM GDB TO MSSQL
+--USE OGR2OGR
+--LOAD INTO SCHEMA: GIS
+/*
+E:\OSGeo4W64\bin\ogr2ogr.exe -f MSSQLSpatial "MSSQL:server=sql2014a8;database=spacecore;trusted_connection=yes" E:\data\urbansim_data_development\scheduled_development\Parcels\urbansim_schedev_parcel_civGQ_add.shp -nln schedev_parcel_civGQ_add -lco SCHEMA=GIS -lco OVERWRITE=YES -OVERWRITE
+*/
+;
+--FIX SRID
+SELECT DISTINCT ogr_geometry.STSrid FROM [GIS].[schedev_parcel_civgq_add];
+UPDATE [GIS].[schedev_parcel_civgq_add] SET ogr_geometry.STSrid = 2230;		--NAD83 / California zone 6 (ftUS)
+
+--ADD COLUMN AND SET TO 0
+ALTER TABLE urbansim.scheduled_development_parcels
+ADD civGQ int NULL;
+
+UPDATE urbansim.scheduled_development_parcels
+SET civGQ = 0;
+
+--CHECK FOR GQ PARCELS DUPLICATES AND DELETE
+SELECT *
+FROM urbansim.scheduled_development_parcels AS sdp
+WHERE EXISTS
+	(SELECT parcel_id 
+	FROM GIS.schedev_parcel_civgq_add  AS sda
+	WHERE sda.parcel_id = sdp.parcel_id)
+
+--Msg 2627, Level 14, State 1, Line 99
+--Violation of PRIMARY KEY constraint 'pk_urbansim_scheddevparcels_parcel_id'. Cannot insert duplicate key in object 'urbansim.scheduled_development_parcels'. The duplicate key value is (5209364).
+
+DELETE
+FROM urbansim.scheduled_development_parcels
+WHERE parcel_id IN(5209364, 5294150)
+
+
+--INSERT GQ PARCELS
+INSERT INTO urbansim.scheduled_development_parcels
+SELECT
+	site_id
+	,parcel_id
+	,capacity3
+	,sfu_effect
+	,mfu_effect
+	,mhu_effect
+	,notes
+	,editor
+	,ogr_geometry
+	,civgq
+FROM GIS.schedev_parcel_civgq_add AS sda
+ORDER BY
+	site_id
+	,parcel_id
+;
+
+--FIX GQ FOR PARCEL 5098007
+SELECT *
+FROM urbansim.scheduled_development_parcels
+WHERE parcel_id = 5098007
+;
+
+UPDATE urbansim.scheduled_development_parcels
+SET civGQ = 138
+	,notes= 'Mission Cov apt and senior housing'
+WHERE parcel_id = 5098007
+;
+
+
+--***************************************************************************
+--CHECKS
+
+--SITE LEVEL
+SELECT
+	usp.site_id AS site_id_usp
+	,sdp.site_id AS site_id_sdp
+	,SUM(capacity_2) AS capacity_2
+	,SUM(capacity_3) AS capacity_3
+	,SUM(COALESCE(capacity_3, capacity_2)) AS cap
+FROM urbansim.urbansim.parcel AS usp
+FULL OUTER JOIN urbansim.urbansim.scheduled_development_parcel AS sdp
+	ON usp.parcel_id = sdp.parcel_id
+WHERE usp.site_id IS NOT NULL
+AND sdp.site_id IS NULL
+GROUP BY
+	sdp.site_id
+	,usp.site_id
+ORDER BY
+	sdp.site_id
+	,usp.site_id
+
+
+--PARCEL LEVEL
+SELECT
+	usp.site_id AS site_id_usp
+	,sdp.site_id AS site_id_sdp
+	,usp.parcel_id AS parcel_id_usp
+	,sdp.parcel_id AS parcel_id_sdp
+	,capacity_2
+	,capacity_3
+	,COALESCE(capacity_3, capacity_2) AS cap
+FROM urbansim.urbansim.parcel AS usp
+FULL OUTER JOIN urbansim.urbansim.scheduled_development_parcel AS sdp
+	ON usp.parcel_id = sdp.parcel_id
+WHERE usp.site_id IS NOT NULL
+--WHERE usp.site_id = 15016
+ORDER BY
+	usp.site_id
+	,usp.parcel_id
+
