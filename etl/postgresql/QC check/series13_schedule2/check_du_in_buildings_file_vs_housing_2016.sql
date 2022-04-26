77f1200256e9c5556357a0e2767a08a056a878c4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-27 07:04:22 +0800

    Delete etl directory

--- etl/postgresql/QC check/series13_schedule2/check_du_in_buildings_file_vs_housing_2016.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT jurisdiction_id, 
-	schedule_id, 
-       sum(buildings_res_units) buildings_res_units 
-FROM urbansim_output.res_capacity
-GROUP BY jurisdiction_id, schedule_id
-ORDER BY jurisdiction_id, schedule_id  
--- etl/postgresql/QC check/zoning_schedule1/check_du_in_buildings_file_vs_housing_2016.sql
+++
@@ -1,5 +0,0 @@
-﻿SELECT jurisdiction_id, 
-       sum(buildings_res_units) buildings_res_units 
-FROM urbansim_output.res_capacity_zoning
-GROUP BY jurisdiction_id 
-ORDER BY jurisdiction_id  
