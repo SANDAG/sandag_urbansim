2052aca37eae1dfd1c53e8e2154cf3aedd5bc353
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-27 08:23:33 +0800

    Delete etl directory

--- etl/postgresql/QC check/series13_schedule2/check_overall_growth_sr13_to_buildings_res_units.sql
+++
@@ -1,5 +0,0 @@
-﻿SELECT sum(buildings_res_units) as buildings_res_units, 
-       sum(sr13_du) as sr13_du, 
-       sum(buildings_res_units) - sum(sr13_du) as difference
-FROM urbansim_output.res_capacity
-WHERE schedule_id = 2;
--- etl/sql/tables/assessor_par_copytable.sql
+++
@@ -1,3 +0,0 @@
-SELECT * 
-INTO spacecore.input.assessor_par 
-FROM  OPENQUERY(sql2014b8, 'SELECT * FROM RM.dbo.ASSESSOR_PAR') 
